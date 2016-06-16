2cac8bc43b79bcb5ebc8ff2948746b4b0ed6bff5
Louis Godfrey <lginn26@users.noreply.github.com>
2016-06-16 14:22:57 -0400

    Update FNITPS #2.py

--- FNITPS #2.py
+++ FNITPS #2.py
@@ -1,2 +1,79 @@
 import random
 
+# Me/Atributes
+You = "Me"
+You_intoxication = 0
+Time = 0.0
+
+#Threats
+py_blue = 'Anemetronic'
+py_blue_AI= random.randint(1, 10)
+py_yellow = 'Anemetronic'
+py_yellow_AI= random.randint(1, 10)
+little_malware = 'Anemetronic'
+Little_malware = random.randint(1, 10)
+
+#Areas
+E11 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+E21 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+E31a = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+E31b = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+B2 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+C2 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+D2 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone'
+
+}
+A2 = {
+    "py blue presence": False,
+    'gass_levels': 0,
+    'presence_message': "A shadow has appeared in the door way",
+    'left message': 'The shadow is gone',
+    'you': True
+
+}
+
+
+while Time >= 6.0 and not A2["py blue presence": True] and not A2['py yellow presence':True]  and not  A2['little_malware':
+True] and not You_intoxication > 10:
+
+
