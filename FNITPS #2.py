6c3ed2acec25ffb97fc6efaa3cd5c4a35c1680a8
Louis Godfrey <lginn26@users.noreply.github.com>
2016-06-21 16:41:13 -0400

    Made "You" into a dictionary, and gave it the action, gass levels, and the animetornics presents tag. Did the same for all Animetronics and gave them the path(WIP), gass tollerents, and ai tags. Made all locations into dictionartys and gave them the present animetronics, gass levels, and messages tag.

--- FNITPS #2.py
+++ FNITPS #2.py
@@ -1,79 +1,83 @@
 import random
 
 # Me/Atributes
-You = "Me"
-You_intoxication = 0
-Time = 0.0
+You = {
+     "action": input('type "pg" then a number 1-4 to pump gass into a certian part of the room, and type.'
+                     'You can also type "mask" to clear out your gass levels'),
+     'gass levels' : 0,
+     "py blue presence" : False,
+     "py yellow presence" : False,
+     "little malware presence" : False
+}
+
 
 #Threats
-py_blue = 'Anemetronic'
-py_blue_AI= random.randint(1, 10)
-py_yellow = 'Anemetronic'
-py_yellow_AI= random.randint(1, 10)
-little_malware = 'Anemetronic'
-Little_malware = random.randint(1, 10)
-
+py_blue = {
+    'path': pass,
+    'gass tolerents': 30,
+    'ai':random.randint(1,10)
+}
+py_yellow = {
+    'path': pass,
+    'gass tolerents': 60,
+    'ai': random.randint(1,10)
+}
+little_malware = {
+    'path': pass,
+    'gass tolerents': 90,
+    'ai': random.randint(1,10)
+}
 #Areas
 E11 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
+    "Present_animetronics":{
+        'py_blue_presents': False,
+        'py_yellow_presents': False,
+        'little_malware_presents': False
+    },
+    'gass_levels':0,
+    'messages':["A shadow has appeared in the door way", "The shadow has left"]
 
 }
 E21 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
+    "Present_animetronics":{
+        'py_blue_presents': False,
+        'py_blue_presents': False,
+        'py_blue_presents': False
+    },
+    'gass_levels':0,
+    'messages':["A yellow figure stands in the hall", "The yellow object is gone"]
 
 }
 E31a = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
+    "Present_animetronics":{
+        'py_blue_presents': False,
+        'py_blue_presents': False,
+        'py_blue_presents': False
+    },
+    'gass_levels':0,
+    'messages':["Somthing little is standing on the left side of the cave area", "The tiny object has retreated back "
+                                                                                  "into the cave"]
 
 }
 E31b = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
+    "Present_animetronics":{
+        'py_blue_presents': False,
+        'py_blue_presents': False,
+        'py_blue_presents': False
+    },
+    'gass_levels': E31a['gass_levels'],
+    'messages':["Somthing little is standing on the right side of the cave area", "The tiny object has retreated back "
+                                                                                  "into the cave"]
 
 }
-B2 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
+B2 =
+C2 =
+D2 =
+Time = 0.0
 
-}
-C2 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
-
-}
-D2 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone'
-
-}
-A2 = {
-    "py blue presence": False,
-    'gass_levels': 0,
-    'presence_message': "A shadow has appeared in the door way",
-    'left message': 'The shadow is gone',
-    'you': True
-
-}
 
 
 while Time >= 6.0 and not A2["py blue presence": True] and not A2['py yellow presence':True]  and not  A2['little_malware':
-True] and not You_intoxication > 10:
+True] and not You['gass levels'] >= 10:
 
 
