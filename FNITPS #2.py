555a75cf9935469d9a7b9902034587f9c72d545c
Ben Godfrey <BenJetson@users.noreply.github.com>
2016-06-27 18:13:43 -0400

    Fixed spelling and style guide errors. Renamed some variables/keys to make better contextual sense.

--- FNITPS #2.py
+++ FNITPS #2.py
@@ -1,112 +1,118 @@
 import random
 
-# Me/Atributes
+# Me/Attributes
 You = {
-     "action": input('type "pg" then a number 1-4 to pump gass into a certian part of the room, and type.'
-                     'You can also type "mask" to clear out your gass levels'),
-     'gass levels' : 0,
-     "py blue presence" : False,
-     "py yellow presence" : False,
-     "little malware presence" : False
+     "action": input('type "pg" then a number 1-4 to pump gas into a certain part of the room, and type. ' +
+                     'You can also type "mask" to clear out your gas levels'),
+     'gas levels': 0,
+     "py blue presence": False,
+     "py yellow presence": False,
+     "little malware presence": False
 }
 
 
-#Threats
+# Threats
 py_blue = {
     'path': None,
-    'gass tolerents': 30,
-    'ai':random.randint(1,10)
+    'gas tolerance': 30,
+    'ai': random.randint(1, 10)
 }
 py_yellow = {
     'path': None,
-    'gass tolerents': 60,
-    'ai': random.randint(1,10)
+    'gas tolerance': 60,
+    'ai': random.randint(1, 10)
 }
 little_malware = {
     'path': None,
-    'gass tolerents': 90,
-    'ai': random.randint(1,10)
+    'gas tolerance': 90,
+    'ai': random.randint(1, 10)
 }
-#Areas
+# Areas
 E11 = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["A shadow has appeared in the door way", "The shadow has left"]
+    'gas levels': 0,
+    'messages': ["A shadow has appeared in the door way", 
+                 "The shadow has left"]
 
 }
 E21 = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["A yellow figure stands in the hall", "The yellow object is gone"]
+    'gas levels': 0,
+    'messages': ["A yellow figure stands in the hall", 
+                 "The yellow object is gone"]
 
 }
 E31a = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["Somthing little is standing on the left side of the cave area", "The tiny object has retreated back "
-                                                                                  "into the cave"]
+    'gas levels': 0,
+    'messages': ["something little is standing on the left side of the cave area", 
+                 "The tiny object has retreated back into the cave"]
 
 }
 E31b = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels': E31a['gass_levels'],
-    'messages':["Somthing little is standing on the right side of the cave area", "The tiny object has retreated back "
-                                                                                  "into the cave"]
+    'gas levels': E31a['gas levels'],
+    'messages': ["something little is standing on the right side of the cave area",
+                 "The tiny object has retreated back into the cave"]
 
 }
 B2 = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["You here rusiling behind the garbage cans", "Rattiling comes from the garbage cans"]
+    'gas levels': 0,
+    'messages': ["You here rustling behind the garbage cans",
+                 "Rattling comes from the garbage cans"]
 
 }
 C2 = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["You feel a blue presence watching you", "You feel a yellow presence watching you", 'Somthing little is'
-                                                                                                    'in the tiny space'
-                                                                                                    'behind you']
+    'gas levels': 0,
+    'messages': ["You feel a blue presence watching you", 
+                 "You feel a yellow presence watching you", 
+                 'Something little is in the tiny space behind you']
 
 }
 D2 = {
-    "Present_animetronics":{
-        'py_blue_presents': False,
-        'py_yellow_presents': False,
-        'little_malware_presents': False
+    "present animatronics": {
+        'py blue presence': False,
+        'py yellow presence': False,
+        'little malware presence': False
     },
-    'gass_levels':0,
-    'messages':["Somthing lunged beind the tool rack"]
+    'gas levels': 0,
+    'messages': ["Something lunged behind the tool rack"]
 
 }
+
 Time = 0.0
 
-
-
-while Time >= 6.0 and not You["py yellow presence":True] and not You["py blue presence":True] and not You["little malware presence":True] and not You['gass levels':10] >= 10:
-    You["action"]
-
-
+while (Time >= 6.0 and 
+       not You["py yellow presence":True] and 
+       not You["py blue presence":True] and 
+       not You["little malware presence":True] and 
+       not You['gas levels':10] >= 10):
+    
+    pass # The line below seems to do nothing. Was that intentional?
+    # You["action"]
