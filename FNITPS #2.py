a9aa11ec11ec0df7892aa91cf554c97ad8a347d1
Louis Godfrey <lginn26@users.noreply.github.com>
2016-07-06 20:41:32 -0400

    Removed unnessesary content. Added if statments.

--- FNITPS #2.py
+++ FNITPS #2.py
@@ -2,9 +2,10 @@ import random
 
 # Me/Attributes
 You = {
-     "action": input('type "pg" then a number 1-4 to pump gas into a certain part of the room, and type. ' +
-                     'You can also type "mask" to clear out your gas levels,' +
-                     'or type anything else to sit and wait.'),
+     "action": input('''type "pg" then a number 1-4 to pump gas into a certain part of the room and control how much
+                     gas you pump by leaving a space then a number 1-3, type.
+                     You can also type "mask" to clear out your gas levels,
+                     or type anything else to sit and wait.'''),
      'gas levels': 0,
      "py blue presence": False,
      "py yellow presence": False,
@@ -14,17 +15,16 @@ You = {
 
 # Threats
 py_blue = {
-    'path': None,
     'gas tolerance': 30,
     'ai': random.randint(1, 10)
 }
+
+
 py_yellow = {
-    'path': None,
     'gas tolerance': 60,
     'ai': random.randint(1, 10)
 }
 little_malware = {
-    'path': None,
     'gas tolerance': 90,
     'ai': random.randint(1, 10)
 }
@@ -36,7 +36,7 @@ E11 = {
         'little malware presence': False
     },
     'gas levels': 0,
-    'messages': ["A shadow has appeared in the door way", 
+    'messages': ["A shadow has appeared in the door way",
                  "The shadow has left"]
 
 }
@@ -109,13 +109,57 @@ D2 = {
 
 Time = 0.0
 
-while (Time >= 6.0 and 
+while (Time >= 6.0 and
        not You["py yellow presence":True] and 
        not You["py blue presence":True] and 
        not You["little malware presence":True] and 
        not You['gas levels':10] >= 10):
 
-    input(You['gas levels'])
+    input(You['action'])
 
-    if You['action'] == 'pg1':
-        E11['gas levels']
+    if You['action'] == 'pg1' :
+        amount = input('How much do you want to pump into this area')
+        E11['gas levels'] += amount
+        You['gas levels'] += amount / 30
+        print("You pumped a {} amount of gas into the door way".format(amount))
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    elif You['action'] == 'pg2':
+        amount = input('How much do you want to pump into this area')
+        E21['gas levels'] += amount
+        You['gas levels'] += amount / 30
+        print("You pumped a {} amount of gas into the hall way".format(amount))
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    elif You['action'] == 'pg3':
+        amount = input('How much do you want to pump into this area')
+        E31a['gas levels'] += amount
+        You['gas levels'] += amount / 30
+        print("You pumped a {} amount of gas into the cave".format(amount))
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    elif You['action'] == 'pg3':
+        amount = input('How much do you want to pump into this area')
+        E31b['gas levels'] += amount
+        You['gas levels'] + amount / 30
+        print("You pumped a {} amount of gas into the cave".format(amount))
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    elif You['action'] == 'pg4':
+        amount = input('How much do you want to pump into this area')
+        C2['gas levels'] += amount
+        You['gas levels'] + amount / 30
+        print("You pumped a {} amount of gas into the door way".format(amount))
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    elif You['action'] == 'mask':
+        You['gas levels'] -= You['gas levels']
+        print("You cleared out the gas in your lungs")
+        print("It is {} and your intoxication levels have reached {}.".format(Time, You['gas levels']))
+    else:
+        print("You didn't do anything.")
+
+    if py_blue['ai'] == 1 or py_blue['ai'] == 2 or py_blue['ai'] == 5 and not E11['py blue presence':True]:
+        E11["present animatronics"]['py blue presence'] = True
+        print(E11['messages'][0])
+    elif E11["present animatronics"]['py blue presence'] == True and not E11['gas levels'] >= 30:
+        E11['py blue presence'] = False
+        You["py blue presence"] = True
+    elif E11["present animatronics"]['py blue presence'] == True and E11['gas levels'] >= 30:
+
+
